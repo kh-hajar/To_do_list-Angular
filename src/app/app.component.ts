5e53af3026c079642afb4d3d9b0c7b4937f28081
kh-hajar <khoukahajar1@gmail.com>
2025-10-17 10:21:33 +0100

    Initialisation du dépôt To_do_list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'To_do_list';
+  task = '';  
+  tasks: { text: string, done: boolean }[] = [];  
+
+  addTask() {
+    const trimmed = this.task.trim();
+
+    if (!trimmed) return;
+
+    if (this.editIndex !== null) {
+      // Mode modification
+      this.tasks[this.editIndex].text = trimmed;
+      this.editIndex = null;  // sortir du mode édition
+    } else {
+      // Mode ajout normal
+      this.tasks.push({ text: trimmed, done: false });
+    }
+
+    this.task = '';
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+  
+  toggleDone(index: number) {
+  this.tasks[index].done = !this.tasks[index].done;
+  }
+
+  editIndex: number | null = null;  // Pour savoir si on est en mode édition
+
+  editTask(index: number) {
+      this.task = this.tasks[index].text;
+      this.editIndex = index;
+  }
 }
